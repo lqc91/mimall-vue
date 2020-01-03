07a6b442eaf44450918205421215fb0240d720de
lufan <ericlufan@163.com>
2020-01-03 19:24:41 +0800

    routes encapsulation

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>login</div>
+  <div>order header</div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'order-header'
 }
 </script>
 
--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <nav-header></nav-header>
+    <order-header></order-header>
     <router-view></router-view>
     <nav-footer></nav-footer>
   </div>
 </template>
 
 <script>
-import NavHeader from '../components/NavHeader'
+import OrderHeader from '../components/OrderHeader'
 import NavFooter from '../components/NavFooter'
 
 export default {
   name: 'order',
   components: {
-    NavHeader,
+    OrderHeader,
     NavFooter
   }
 }
