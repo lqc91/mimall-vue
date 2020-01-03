b615c4eacc32d8776325f1af1fcb31419fd0af29
lufan <ericlufan@163.com>
2020-01-04 04:16:38 +0800

    intercept interface errors with axios.interceptors

--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
 import Vue from "vue";
-import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import App from "./App.vue";
+
+// 根据前端跨域方式调整
+// 此处为代理跨域
+// 若后端接口为 /a/b
+// 前端可通过 devServer.pathRewrite 去除请求 URL 中的 /api
+// /api/a/b => /a/b
+axios.defaults.baseURL = "/api";
+axios.defaults.timeout = 8000;
+
+// 接口错误拦截
+// status: 0 请求成功
+// status: 10 未登录
+axios.interceptors.response.use(response => {
+  let res = response.data;
+  if (res.status === 0) {
+    return res.data;
+  } else if (res.status === 10) {
+    // 路由挂载在 Vue 实例上，在 vue 页面文件中才能使用 this.$router.push 进行页面跳转
+    // 此处无法使用
+    window.location.href = "/#/login";
+  } else {
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
