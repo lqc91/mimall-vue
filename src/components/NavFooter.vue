4ae08d8760e1b31d9e0e6ffdcfe7026f1a21232e
lufan <ericlufan@163.com>
2020-01-03 09:15:28 +0800

    create directory structure & install base plugins

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>nav footer</div>
+</template>
+
+<script>
+export default {
+  name: 'nav-footer'
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>nav header</div>
+</template>
+
+<script>
+export default {
+  name: 'nav-header'
+}
+</script>
+
+<style>
+</style>
